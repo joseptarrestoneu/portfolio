3a7fe7d98d8e954431ee82a70380460e75e03083
joseptarrestoneu <joseptarrestoneu@gmail.com>
2023-10-05 00:32:28 +0200

    Fixed Bugs

--- app2.js
+++ app2.js
@@ -1,3 +1,22 @@
-setInterval(() => {
+let counter = 0;
+let timer = setInterval(() => {
+    let div = document.getElementById("EnterView");
+    div.remove();
+    counter++;
+    if (counter == 1) {
+        clearInterval(timer);
+    }
+}, 3000);
 
-}, 1000);
+function changeFont() {
+    fontFamily = document.getElementById("monospaced").textContent;
+    console.log(fontFamily);
+    
+    if (fontFamily == "Monospaced") {
+        document.getElementById('_font').style.fontFamily = 'Neue Montreal Mono';
+        document.getElementById('monospaced').innerHTML= 'Normal';
+    } else {
+        document.getElementById('_font').style.fontFamily = 'Neue Montreal';
+        document.getElementById('monospaced').innerHTML= 'Monospaced';
+    }
+}
