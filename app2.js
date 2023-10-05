90e43b07ed90588add3c18399d4a172c93b98e40
joseptarrestoneu <joseptarrestoneu@gmail.com>
2023-10-05 16:41:17 +0200

    Add pages html

--- app2.js
+++ app2.js
@@ -1,12 +1,32 @@
 let counter = 0;
-let timer = setInterval(() => {
+let counter2 = 0;
+let opacity = 0.5;
+
+
+if(document.getElementById('page')==null) {
+    let timer2 = setInterval(() => {
+        document.getElementById('_t1').style.opacity = opacity;
+        document.getElementById('_t2').style.opacity = opacity;
+        opacity = opacity - 0.005;
+        counter2++;
+        if (counter2 == 200) {
+            clearInterval(timer2);
+        }
+    }, 12.5);
+    
+    let timer = setInterval(() => {
+        let div = document.getElementById("EnterView");
+        div.remove();
+        counter++;
+        if (counter == 1) {
+            clearInterval(timer);
+        }
+    }, 3000);
+    
+} else {
     let div = document.getElementById("EnterView");
     div.remove();
-    counter++;
-    if (counter == 1) {
-        clearInterval(timer);
-    }
-}, 3000);
+}
 
 function changeFont() {
     fontFamily = document.getElementById("monospaced").textContent;
@@ -20,3 +40,5 @@ function changeFont() {
         document.getElementById('monospaced').innerHTML= 'Monospaced';
     }
 }
+
+
